d77527557ba8fbd0b4c04872cca54cb1f4895729
aleks <gesashka@gmail.com>
2026-02-02 02:45:43 +0400

    Change ByteConnect directory to user home for better file organization

--- src/byteconnect.ts
+++ src/byteconnect.ts
@@ -6,7 +6,7 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 
-const byteconnectDir = __dirname.replace(/\\/g, '/');
+const byteconnectDir = path.join(os.homedir(), '.aydo', 'byteconnect').replace(/\\/g, '/');;
 
 let aydoByteConnectProcess: any;
 
